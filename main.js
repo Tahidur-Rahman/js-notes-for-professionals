3d9a94175f7789145287f89ecd64c6c56241ec62
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2021-01-18 10:44:15 +0600

    formatting console & measuring console time

---
+++ main.js
@@ -0,0 +1,31 @@
+// // 5.1 Measuring time - console.time()
+
+// console.time("response in")
+// alert('1')
+// console.timeEnd("response in")
+// alert('3')
+// console.timeEnd("response in")
+
+// // 5.2 Formatting console output
+
+// // c- like console output
+
+// console.log("%s has %d points",'Sam',100) // Format specifier -- %s and %d
+
+// // %o - expandable dom 
+// // %0 - expandable JS obj 
+// // %c  - css styling 
+
+// // Advanced Styling -- %c
+
+// console.log("%cHello World !",'color:blue;font-size:xx-large'); // xx-large blue text
+
+// // Mulitple styling
+
+// console.log("%cHello %cWorld %c!!",'color:blue',"font-size:40px",''); // 3 diff style
+
+// // Using groups to indent output
+  
+//   console.groupCollapsed(33,3)
+//   console.group(2,2,2)
+//   console.groupEnd()
