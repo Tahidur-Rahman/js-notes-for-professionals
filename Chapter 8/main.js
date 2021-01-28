44b3e9334de02b9d6e6cddaffec7c9be7a155385
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2021-01-28 19:15:31 +0600

    chapter 9 -compledted

--- Chapter 8/main.js
+++ Chapter 8/main.js
@@ -1,33 +1,33 @@
-// // chapter 8 : Date
+// chapter 8 : Date
 
-// console.log(Date())
-// console.log(new Date('05-01-2002')) // with date string
-// // with multiple parameters
-// console.log(new Date(1998,01,05,1,1,1))
+console.log(Date())
+console.log(new Date('05-01-2002')) // with date string
+// with multiple parameters
+console.log(new Date(1998,01,05,1,1,1))
 
-// let now = new Date();
-// console.log(now.toString() === 'Thu Jan 28 2021 12:48:19 GMT+0600 (Bangladesh Standard Time)')
+let now = new Date();
+console.log(now.toString() === 'Thu Jan 28 2021 12:48:19 GMT+0600 (Bangladesh Standard Time)')
 
-// // date in unix epoch
+// date in unix epoch
 
-// let epoch  = new Date(0);
-// console.log(epoch.toISOString())
+let epoch  = new Date(0);
+console.log(epoch.toISOString())
 
-// console.log(new Date(Date.UTC(2020,01)).toISOString())
+console.log(new Date(Date.UTC(2020,01)).toISOString())
 
-// console.log(new Date('2013 Dec 03 12:12:22'))
+console.log(new Date('2013 Dec 03 12:12:22'))
 
-// console.log((new Date()).toTimeString())
-// console.log((new Date()).toDateString())
-// console.log((new Date()).toUTCString())
-// console.log((new Date()).toISOString())
-// console.log((new Date()).toGMTString())
-// console.log((new Date()).toLocaleDateString())
-// console.log((new Date()).toLocaleDateString(['zh','en-US']))
-// let options = {weekday:'long',year:'numeric',month:'long',day:'numeric'}
-// console.log((new Date()).toLocaleDateString([],options))
+console.log((new Date()).toTimeString())
+console.log((new Date()).toDateString())
+console.log((new Date()).toUTCString())
+console.log((new Date()).toISOString())
+console.log((new Date()).toGMTString())
+console.log((new Date()).toLocaleDateString())
+console.log((new Date()).toLocaleDateString(['zh','en-US']))
+let options = {weekday:'long',year:'numeric',month:'long',day:'numeric'}
+console.log((new Date()).toLocaleDateString([],options))
 
-// convert time to local time from any timezone
+convert time to local time from any timezone
 
 // Wrong approch
 let anytime = new Date(2000, 0, 3);
@@ -72,3 +72,21 @@ console.log(
 
 console.log(Date.UTC(2000,0,31,12))
 console.log(new Date(Date.UTC(2000,0,31,12)))
+
+
+// Formatting js date
+
+console.log(Date.now())
+console.log(new Date().getFullYear())
+console.log(new Date().getMonth())
+console.log(new Date().getMilliseconds())
+console.log((new Date()).toUTCString())
+console.log((new Date()).getTime())
+
+// increment a date object
+
+let date = new Date(2020,1,1);
+date.setDate(date.getDate() + 232)
+console.log(date)
+
+console.log(date.toJSON())
