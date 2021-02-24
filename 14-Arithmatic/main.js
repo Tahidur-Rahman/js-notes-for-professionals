e1bab79c1f985c1980fcd958dc23654294a1b858
Tahid <mdtahidurrahman20@gmail.com>
2021-02-24 17:58:30 +0600

    ARithmatic

---
+++ 14-Arithmatic/main.js
@@ -0,0 +1,26 @@
+
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MAX_VALUE)
+console.clear()
+
+// Trucating
+
+console.log(2.3 | 0)
+console.log(Math.trunc(2.2))
+console.log(Math.trunc(-2.2))
+
+// Trigonometry 
+console.log(Math.sin(2))
+
+// Bitwise Operator
+console.log(101 | 11)
+console.log(101 & 11)
+console.log(~10)
+
+console.log(Math.pow(64,1/3))
+
+console.log((Math.random() * (20-10)) + 10)
+console.clear()
+
+// Chapter 15 -  Bitwise Operators
+
